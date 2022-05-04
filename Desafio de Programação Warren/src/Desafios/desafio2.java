f665253c92d6d38fadcc3744ca14eec12760196c
Guilherme Maia <Guilherme Maia@DESKTOP-PKKRSUO>
2022-05-04 14:46:10 -0200

    Deixando arquivo com apenas os desafios

--- Desafio de Programação Warren/src/Desafios/desafio2.java
+++ Desafio de Programação Warren/src/Desafios/desafio2.java
@@ -5,43 +5,30 @@ import javax.swing.JOptionPane;
 public class desafio2 {
 
 	public static void main(String[] args) {
-		
+
 		int minimo = 3;
 		int alunos = 0;
 		String[] tempoChegada = new String[5];
+		int[] chegadaAluno = new int[5];
 
-		//Solução com Inputs
-		
 		tempoChegada[0] = JOptionPane.showInputDialog("Em que horário chegou o aluno 1?");
-		double chegadaAluno1 = Double.parseDouble(tempoChegada[0]);
+		chegadaAluno[0] = Integer.parseInt(tempoChegada[0]);
 		tempoChegada[1] = JOptionPane.showInputDialog("Em que horário chegou o aluno 2?");
-		double chegadaAluno2 = Double.parseDouble(tempoChegada[1]);
+		chegadaAluno[1] = Integer.parseInt(tempoChegada[1]);
 		tempoChegada[2] = JOptionPane.showInputDialog("Em que horário chegou o aluno 3?");
-		double chegadaAluno3 = Double.parseDouble(tempoChegada[2]);
+		chegadaAluno[2] = Integer.parseInt(tempoChegada[2]);
 		tempoChegada[3] = JOptionPane.showInputDialog("Em que horário chegou o aluno 4?");
-		double chegadaAluno4 = Double.parseDouble(tempoChegada[3]);
+		chegadaAluno[2] = Integer.parseInt(tempoChegada[3]);
 		tempoChegada[4] = JOptionPane.showInputDialog("Em que horário chegou o aluno 5?");
-		double chegadaAluno5 = Double.parseDouble(tempoChegada[4]);
+		chegadaAluno[4] = Integer.parseInt(tempoChegada[4]);
 
-		if (chegadaAluno1 <= 0) {
-			alunos = alunos+1;
-			System.out.println("Aluno 1 chegou a tempo, agora temos " + alunos + " alunos em sala");			
-		}if (chegadaAluno2 <= 0) {
-			alunos = alunos+1;
-			System.out.println("Aluno 2 chegou a tempo, agora temos " + alunos + " alunos em sala");			
-		}if (chegadaAluno3 <= 0) {
-			alunos = alunos+1;
-			System.out.println("Aluno 3 chegou a tempo, agora temos " + alunos + " alunos em sala");			
-		}if (chegadaAluno4 <= 0) {
-			alunos = alunos+1;
-			System.out.println("Aluno 4 chegou a tempo, agora temos " + alunos + " alunos em sala");			
-		}if (chegadaAluno5 <= 0) {
-			alunos = alunos+1;
-			System.out.println("Aluno 5 chegou a tempo, agora temos " + alunos + " alunos em sala");			
-		}if (alunos >= minimo) {
-			System.out.println("Aula Normal");
-		}else {System.out.println("Aula Cancelada por falta de alunos");}
-
+		for (int i = 0; i < chegadaAluno.length; i++) {
+			if (chegadaAluno[i] <= 0) {
+				alunos = alunos + 1;
+				System.out.println("O aluno " + (i + 1)  + " chegou a tempo, agora temos " + alunos + " alunos em sala");
+			}
+		}
+		System.out.println((alunos >= minimo)? "Aula Normal" : "Aula Cancelada");
 	}
 
 }
