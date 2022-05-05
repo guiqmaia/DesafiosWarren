c84a9a213e8d105d602d898572ea26ec69c9d773
Guilherme Maia <Guilherme Maia@DESKTOP-PKKRSUO>
2022-05-04 22:04:49 -0200

    att

--- Desafio de Programação Warren/src/Desafios/desafio2.java
+++ Desafio de Programação Warren/src/Desafios/desafio2.java
@@ -11,24 +11,18 @@ public class desafio2 {
 		String[] tempoChegada = new String[5];
 		int[] chegadaAluno = new int[5];
 
-		tempoChegada[0] = JOptionPane.showInputDialog("Em que horário chegou o aluno 1?");
-		chegadaAluno[0] = Integer.parseInt(tempoChegada[0]);
-		tempoChegada[1] = JOptionPane.showInputDialog("Em que horário chegou o aluno 2?");
-		chegadaAluno[1] = Integer.parseInt(tempoChegada[1]);
-		tempoChegada[2] = JOptionPane.showInputDialog("Em que horário chegou o aluno 3?");
-		chegadaAluno[2] = Integer.parseInt(tempoChegada[2]);
-		tempoChegada[3] = JOptionPane.showInputDialog("Em que horário chegou o aluno 4?");
-		chegadaAluno[3] = Integer.parseInt(tempoChegada[3]);
-		tempoChegada[4] = JOptionPane.showInputDialog("Em que horário chegou o aluno 5?");
-		chegadaAluno[4] = Integer.parseInt(tempoChegada[4]);
+		for (int i = 0; i < 5; i++) {
+			tempoChegada[i] = JOptionPane.showInputDialog("Em que horário chegou o aluno " + (i + 1) + "?");
+			chegadaAluno[i] = Integer.parseInt(tempoChegada[i]);
+		}
 
 		for (int i = 0; i < chegadaAluno.length; i++) {
 			if (chegadaAluno[i] <= 0) {
 				alunos = alunos + 1;
-				System.out.println("O aluno " + (i + 1)  + " chegou a tempo, agora temos " + alunos + " alunos em sala");
+				System.out.println("O aluno " + (i + 1) + " chegou a tempo, agora temos " + alunos + " alunos em sala");
 			}
 		}
-		System.out.println((alunos >= minimo)? "Aula Normal" : "Aula Cancelada");
+		System.out.println((alunos >= minimo) ? "Aula Normal" : "Aula Cancelada");
 	}
 
 }
--- Desafio de Programação Warren/src/Desafios/desafio3.java
+++ Desafio de Programação Warren/src/Desafios/desafio3.java
@@ -13,13 +13,15 @@ public class desafio3 {
 		int[] v1 = new int[3];
 		
 		for (int i = 0; i < 3; i++) {
-			v[i] = JOptionPane.showInputDialog("Digite o número que irá armazenar a posição: " + i);
+			v[i] = JOptionPane.showInputDialog("Digite o número" + (i + 1) + " da lista");
 			v1[i] = Integer.parseInt(v[i]);
 		}
 		
 		System.out.println("Valor de n é igual a " + n1);
 		
 		if (v1[0] + v1[1] + v1[2] == n1) {
+			System.out.println("[ " + v1[0] + ", " + v1[1] + ", " + v1[2] + "]");
+		}if (v1[0] + v1[0] + v1[1] == n1) {
 			System.out.println("[ " + v1[0] + ", " + v1[1] + ", " + v1[2] + "]");
 		}if (v1[0] + v1[1] + v1[1] == n1) {
 			System.out.println("[ " + v1[0] + ", " + v1[1] + ", " + v1[1] + "]");
